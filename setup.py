a0ff7c7a3a9371f6bd460e5c296fd35cca4199ac
Andrea Spadaccini <andrea.spadaccini@microsoft.com>
2018-11-18 18:12:25 +0000

    Specify Python version, bump version

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ setup(
     entry_points={
         "console_scripts": ["cmd-call-graph = callgraph.callgraph:main"]
     },
-    version=0.1,
+    version="0.1.2",
     author="Andrea Spadaccini",
     author_email="andrea.spadaccini@gmail.com",
     description="A simple tool to generate a call graph for calls within Windows CMD (batch) files.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Microsoft/cmd-call-graph",
+    python_requires=">=2.7, >=3.2",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
