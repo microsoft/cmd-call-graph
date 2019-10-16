3e2e27784e3d3447fbfc4e6a6fb3c4590ef23a4b
ankitsaxena21 <asrokx917@gmail.com>
2019-10-16 11:42:39 -0700

    Updat cmd-call-graph.py

--- cmd-call-graph.py
+++ cmd-call-graph.py
@@ -1,8 +1,8 @@
 # cmd-call-graph.py
 #
-# Outputs a call graph for the given CMD / batch file in DOT (https://en.wikipedia.org/wiki/DOT_(graph_description_language)).
+# Outputs a call graph for the given CMD / batch file in DOT
+# (https://en.wikipedia.org/wiki/DOT_(graph_description_language)).
 
 from callgraph.callgraph import main
-    
-if  __name__ == "__main__":
+if __name__ == "__main__":
     main()
